a454e049fdf72046e0f66b6a5cb8cd0083c27252
Acivev <37246611+Acivev@users.noreply.github.com>
2022-03-13 00:07:56 +0100

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-screen -X -S osudroidSkin
-
-cd /var/www/osudroid/Skin-Website
 git pull https://github.com/osudroid/Skin-Website.git
 npm run build
-npm start
-
-cd /var/www/osudroid/Skin-Website
-screen -S osudroidSkin
-
-while true  
-do  
-  acd_cli sync  
-  sleep 900  
-done
-
+PORT=6969 npm start
